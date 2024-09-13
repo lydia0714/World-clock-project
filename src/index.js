e73e2c6f01ea6df585cfbe0d6488c6d9fb7a666e
lydia0714 <lvhunga@yahoo.com>
2024-09-13 20:40:07 +0200

    added the functionality of select

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-setInterval (function () {
+function updateTime(){
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesTimeElement = document.querySelector("#los-angeles .time");
 let losAngelesDateElement = document.querySelector("#los-angeles .date");
 let losAngelesTime = moment ().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format ("MMMM DD YYYY");
-losAngelesTimeElement.innerHTML = ` ${losAngelesTime.format(
-    "h:mm:ss"
-)} <small>${losAngelesTime.format("A")}</small>`;
-},1000);
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss[<small>]A[</small>]"
+);
 
 let sydneyElement = document.querySelector("#sydney");
 let sydneyTimeElement = document.querySelector("#sydney .time");
@@ -16,6 +14,27 @@ let sydneyDateElement = document.querySelector("#sydney .date");
 let sydneyTime = moment ().tz("Oceania/Sydney");
 
 sydneyDateElement.innerHTML = sydneyTime.format ("MMMM DD YYYY");
-sydneyTimeElement.innerHTML = ` ${sydneyTime.format(
-    "h:mm:ss"
-)} <small>${sydneyTime.format("A")}</small>`;
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>]A[</small>]"
+);
+}
+
+function updateCity (event) {
+let cityTimeZone = event.target.value;
+let cityName =cityTimeZone.replace ("_", " ").split("/")[1]
+let cityTime = moment ().tz(cityTimeZone)
+let citiesElement = document.querySelector("#cities")
+citiesElement.innerHTML = `
+<div class="city">
+      <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM DD YYYY")}</div>
+    </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>`;
+
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector ("#city")
+citiesSelectElement.addEventListener("change", updateCity)
